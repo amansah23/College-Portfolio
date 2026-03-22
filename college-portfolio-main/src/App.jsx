23454bf2d54689bcc66983d02c39ab1e2652430d
Aman Kumar Sah <amansah23@gmail.com>
2026-03-22 21:40:01 +0530

    uploading file

---
+++ college-portfolio-main/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Hero from "./components/Hero"
+import Skills from "./components/Skills"
+import Projects from "./components/Projects"
+import Certifications from "./components/Certifications"
+import University from "./components/University"
+// import Achievements from "./components/Achievements"
+import Resume from "./components/Resume"
+import Footer from "./components/Footer"
+import Navigation from './components/Navigation'
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+      <Skills />
+      <Projects />
+      <Certifications />
+      <University />
+      {/* <Achievements /> */}
+      <Resume />
+      <Footer />
+    </>
+  )
+}
+
+export default App
